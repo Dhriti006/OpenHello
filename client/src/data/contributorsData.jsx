7f9e24dafc98844fa2aac655da5f47b59393bebf
Dhriti Mestry <dhritimestry2006@gmail.com>
2025-09-17 13:30:36 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,8 +1,8 @@
 export const contributorsData = [
   {
-    name: "Siddesh Navthale",
-    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
-    githubUrl: "https://github.com/sidxhdev",
+    name: "Dhriti Mestry",
+    photoUrl: "https://avatars.githubusercontent.com/u/220616504?s=400&u=d09e830a1ec7875b7892b33e40f65093ea017977&v=4",
+    githubUrl: "https://github.com/Dhriti006",
   },
   
  
